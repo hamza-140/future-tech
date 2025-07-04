4cd3ca8212b963d64af5daecde22e3777e726526
Hamza <sahamzashah19@gmail.com>
2025-07-04 14:47:39 +0500

    added new components

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,31 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import Home from "../pages/Home";
-import { useSelector } from "react-redux";
 import AppLayout from "../layouts/AppLayout";
 
 const AppRoutes = () => {
-  const user = useSelector((state) => state.auth.user);
-
   return (
     <BrowserRouter>
       <Routes>
-        {/* Protected Layout */}
-        <Route element={user ? <AppLayout /> : <Navigate to="/login" />}>
+        {/* Always show Home inside AppLayout */}
+        <Route element={<AppLayout />}>
           <Route path="/" element={<Home />} />
         </Route>
 
         {/* Public Routes */}
-        <Route
-          path="/login"
-          element={!user ? <Login /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/signup"
-          element={!user ? <Signup /> : <Navigate to="/" />}
-        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
